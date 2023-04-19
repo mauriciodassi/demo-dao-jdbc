922360ffc2220a92a8c8dec3bac9285309233063
Mauricio Dassi <maupossari@gmail.com>
2023-04-19 11:08:21 -0300

    FindById impl

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,8 +1,7 @@
 package application;
 
-import java.util.Date;
-
-import model.entities.Department;
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Seller;
 
 public class Program {
@@ -10,9 +9,12 @@ public class Program {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		Department obj = new Department(1, "Books");
 		
-		Seller seller = new Seller(21,"Bob","bob@gmail.com", new Date(),2000.0, obj);
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
+		Seller seller = sellerDao.findById(3);
+		
 		
 		System.out.println(seller);
 		
