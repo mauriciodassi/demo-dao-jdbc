9c6f0775c3992e627af03215e3d32ec7bc7bae07
Mauricio Dassi <maupossari@gmail.com>
2023-04-20 10:03:02 -0300

    reusando instanciação

--- src/application/Program.java
+++ src/application/Program.java
@@ -7,19 +7,15 @@ import model.entities.Seller;
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		
-		
+						
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
+		System.out.println("=== TEST1: seller findById ===");
+		
 		Seller seller = sellerDao.findById(3);
-		
-		
+				
 		System.out.println(seller);
-		
-		
-
+				
 	}
 
 }
